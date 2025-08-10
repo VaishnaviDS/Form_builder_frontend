70f36d85c08ca4f3c4c654d70e1ca625faed906b
Vaishnavi Sakpal <142401260+VaishnaviDS@users.noreply.github.com>
2025-08-10 08:54:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode,React } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-export const server="http://localhost:10000"
+export const server="https://form-builder-server-95bs.onrender.com"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
